d7c0122cec3d57c5787dde6125e581c4f44c8e92
nedved942 <pawelkoziol77@wp.pl>
2023-10-11 23:16:57 +0200

    Wstępna implementacja programu.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 from datetime import date, timedelta
 
 print("*** Obsługa biblioteki zewnętrznej ***")
@@ -9,8 +10,17 @@ latitude = "50"
 longitude = "50"
 searched_date = "2023-10-11"
 URL = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude" \
-      f"={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
+      f"={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&" \
+      f"start_date={searched_date}&end_date={searched_date}"
 
 data = requests.get(URL)
 data_dict = data.json()
-print(data_dict)
+pprint(data_dict)
+rain_sum = data_dict['daily']['rain_sum'][0]
+
+if rain_sum > 0:
+    print("Będzie padać.")
+elif rain_sum == 0:
+    print("Nie będzie padać.")
+else:
+    print("Nie wiem.")
